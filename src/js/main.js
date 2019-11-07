c4b51626abeb3a84db1ae074b20f498fc42e7e75
cafesao <gabriel.diasdutra2@gmail.com>
2019-11-07 13:28:57 -0300

    [CS] JS Principal V1.1

--- src/js/main.js
+++ src/js/main.js
@@ -1,34 +1,49 @@
-import { PesquisarCarro, LimparResultado } from "./funcoes_Pesquisar"
+import { PesquisarCarro } from "./funcoes_Pesquisar"
 import { PrepararTags } from "./funcoes_Adicionar"
+
+function LimparCampos(array) {
+    for(let i = 0; i < array.length; i++) {
+        array[i].value = ''
+    }
+}
+
+function AdicionarEnter(elemento, funcao) {
+    elemento.addEventListener('keydown', (e) => {
+        if (e.keyCode === 13) {
+            funcao()
+        }
+    })
+}
+
+function AdicionarClickBotao(elemento, funcao) {
+    elemento.addEventListener('click', funcao)
+}
+
+function LimparDivHTML() {
+    div.innerHTML = ''
+}
 
 //PESQUISAR
 
 let modeloInputPesquisar = document.querySelector('div#pesquisar input#modeloInput')
 let pesquisarButton = document.querySelector('button#pesquisarButton')
 let apagarButton = document.querySelector('button#apagarButton')
+let div = document.querySelector('div#resultado')
 
-pesquisarButton.addEventListener('click', Pesquisar)
-apagarButton.addEventListener('click', LimparResultado)
+AdicionarClickBotao(pesquisarButton, Pesquisar)
+AdicionarClickBotao(apagarButton, LimparDivHTML)
 
-modeloInputPesquisar.addEventListener("keydown", (e) => {
-    if (e.keyCode === 13) { 
-        Pesquisar()
-    }
-})
-
+AdicionarEnter(modeloInputPesquisar, Pesquisar)
 
 function Pesquisar() {
     if(modeloInputPesquisar.value.length === 0) {
         alert('[ERRO] Por favor, prencha o campo de modelo')
     }
     else {
+        LimparDivHTML()
         PesquisarCarro(modeloInputPesquisar.value)
-        LimparPesquisar()
+        LimparCampos([modeloInputPesquisar])
     }
-}
-
-function LimparPesquisar() {
-    modeloInputPesquisar.value = ''
 }
 
 //ADICIONAR
@@ -43,24 +58,17 @@ let defeitosInputAdicionar = document.querySelector('div#adicionar input#defeito
 
 let adicionarButton = document.querySelector('button#adicionarButton')
 
-adicionarButton.addEventListener('click', Adicionar)
+AdicionarClickBotao(adicionarButton, Adicionar)
 
 function Adicionar() {
     if(modeloInputAdicionar.value.length === 0 || marcaInputAdicionar.value.length === 0 || anoInputAdicionar.value.length === 0 || precoInputAdicionar.value.length === 0) {
         alert('[ERRO] Por favor, prencha todos os campos obrigatorios!')
     }
     else {
+        if(placaInputAdicionar.length === 0) {
+            placaInputAdicionar = 0
+        }
         PrepararTags(modeloInputAdicionar.value, marcaInputAdicionar.value, placaInputAdicionar.value, anoInputAdicionar.value, kmInputAdicionar.value, precoInputAdicionar.value, defeitosInputAdicionar.value)
-        LimparAdicionar()
+        LimparCampos([modeloInputAdicionar, marcaInputAdicionar, placaInputAdicionar, anoInputAdicionar, kmInputAdicionar, precoInputAdicionar, defeitosInputAdicionar])
     }
-}
-
-function LimparAdicionar() {
-    modeloInputAdicionar.value = ''
-    marcaInputAdicionar.value = ''
-    placaInputAdicionar.value = ''
-    anoInputAdicionar.value = ''
-    kmInputAdicionar.value = ''
-    precoInputAdicionar.value = ''
-    defeitosInputAdicionar.value = ''
 }
